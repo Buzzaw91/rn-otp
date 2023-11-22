1473ddd7bb03836e80dbbaa0e5cad8f6fd857bd5
Aleksi Ylönen <aleksi.ylonen2@gmailcom>
2023-11-22 15:30:55 +0200

    request opt, v1

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,10 @@
 
 import * as admin from 'firebase-admin'
 import { createUser } from "./create_user";
+import { requestOtp } from './request_otp';
 import serviceAccount from '../service_account.json'
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
 });
 
-export { createUser }
+export { createUser, requestOtp }
---
+++ functions/src/request_otp.ts
@@ -0,0 +1,36 @@
+import { onRequest } from "firebase-functions/v2/https";
+import * as admin from 'firebase-admin';
+import twilio from './twilio'
+
+export const requestOtp = onRequest(async (request, response): Promise<void> => {
+ if (!request.body.phone) {
+   response.status(422).send({ error: 'You must provide a phone number' });
+   return;
+ }
+ const phone = String(request.body.phone).replace(/[^\d]/g, "");
+
+ admin.auth().getUser(phone)
+  .then(userRecord => {
+    const code = Math.floor(Math.floor(Math.random() * 8999 + 1000));
+
+    twilio.messages.create({
+      body: `Your code is ${code}`,
+      to: phone,
+      from: '+358454909714'
+    }, (err) => {
+      if (err) {
+        response.status(422).send({ error: err });
+      } else {
+        admin.database().ref('users/' + phone)
+          .update({ code: code, codeValid: true }, () => {
+            response.send({ success: true });
+          });
+        }
+    });
+  })
+  .catch((err) => {
+    // TODO: Once confirmed working, change message to "User not found"
+    response.status(422).send({ error: err });
+  })
+
+})
