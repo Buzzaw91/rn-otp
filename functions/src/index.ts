027551b7062c397c09913c456b24cf9b984756b7
Aleksi Ylönen <aleksi.ylonen2@gmailcom>
2023-11-22 17:13:56 +0200

    debugging: simple function only

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,15 @@
-
-
 import * as admin from 'firebase-admin'
-import { createUser } from "./create_user";
-import{ requestOtp } from "./request_otp";
-// imporServiceAccount
+import { onRequest } from "firebase-functions/v2/https";
+// import { createUser } from "./create_user";
+// import{ requestOtp } from "./request_otp";
 import serviceAccount from '../service_account.json'
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
 });
 
-export { createUser, requestOtp };
+// export { createUser, requestOtp };
+
+
+exports.simpleTest = onRequest((request, response) => {
+  response.send("This is a simple test function.");
+});
