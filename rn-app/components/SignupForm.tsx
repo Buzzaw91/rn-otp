4db927b2e114484ae0d7960274476a24decc553f
Aleksi Ylönen <aleksi.ylonen2@gmailcom>
2023-11-29 18:23:14 +0200

    http requests for signup

--- rn-app/components/SignupForm.tsx
+++ rn-app/components/SignupForm.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Input, Button, } from '@rneui/base';
+import axios from 'axios';
+import { CREATE_USER, REQUEST_OTP } from '../utils/constants';
 
 export default function SignupForm() {
+  const [phone, setPhone] = useState('');
+
+  const handleSubmit = async () => {
+    try {
+      await axios.post(CREATE_USER, { phone })
+      await axios.post(REQUEST_OTP, { phone })
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <View>
-      <Input label={'Enter phone number'} />
-      <Button title='Submit'>Sign Up</Button>
+      <Input label={'Enter phone number'} value={phone} onChangeText={phone => setPhone(phone)} />
+      <Button onPress={handleSubmit} title='Submit'>Sign Up</Button>
     </View>
   );
 }
---
+++ rn-app/utils/constants.ts
@@ -0,0 +1,2 @@
+export const CREATE_USER = 'https://createuser-7rxj2ngoiq-uc.a.run.app'
+export const REQUEST_OTP = 'https://requestotp-7rxj2ngoiq-uc.a.run.app'
