090dfc67946451e6f2bbc144f2c1fd33bf942c7b
Aleksi Ylönen <aleksi.ylonen2@gmailcom>
2023-11-29 19:10:33 +0200

    refactor: naming, and handleSubmit

--- rn-app/components/SignupForm.tsx
+++ rn-app/components/SignupForm.tsx
@@ -1,25 +1,40 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Input, Button, } from '@rneui/base';
-import axios from 'axios';
-import { CREATE_USER, REQUEST_OTP } from '../utils/constants';
+import React, { useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Input, Button, } from '@rneui/base'
+import axios from 'axios'
+import { CREATE_USER, REQUEST_OTP } from '../utils/constants'
 
 export default function SignupForm() {
-  const [phone, setPhone] = useState('');
+  const [phone, setPhone] = useState('')
+
+  const isValidPhoneNumber = (phoneNumber: string) => {
+    const phoneRegex = /^\+[1-9]{1}[0-9]{3,14}$/
+
+    return phoneRegex.test(phoneNumber)
+  };
+
 
   const handleSubmit = async () => {
-    try {
-      await axios.post(CREATE_USER, { phone })
-      await axios.post(REQUEST_OTP, { phone })
+    if (!isValidPhoneNumber(phone)) {
+      console.log('Invalid phone number')
+      return; // Exit if the phone number is not valid
     }
-    catch (err) {
-      console.log(err)
+    await createUserAndRequestOtp(phone); // Submit the phone number if it's valid
+  }
+
+  const createUserAndRequestOtp = async (phoneNumber: string) => {
+    try {
+      await axios.post(CREATE_USER, { phone: phoneNumber })
+      await axios.post(REQUEST_OTP, { phone: phoneNumber })
+    } catch (err) {
+      console.error('Error in submitting phone number:', err)
     }
   }
 
+
   return (
     <View>
-      <Input label={'Enter phone number'} value={phone} onChangeText={phone => setPhone(phone)} />
+      <Input placeholder='+123456789' label={'Enter phone number'} value={phone} onChangeText={phone => setPhone(phone)} />
       <Button onPress={handleSubmit} title='Submit'>Sign Up</Button>
     </View>
   );
